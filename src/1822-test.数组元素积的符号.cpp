be09b9be8a208b07d9b471378db82fadae1fb87d
cosette <2052137445@qq.com>
2025-11-20 22:47:49 +0800

    1822.数组元素积的符号

--- src/1822-test.数组元素积的符号.cpp
+++ src/1822-test.数组元素积的符号.cpp
@@ -7,7 +7,7 @@ namespace problem {
 
 auto solution = new problem::Solution();
 
-TEST_CASE("283") {
+TEST_CASE("1822") {
   std::vector<int> nums = {-1, -2, -3, -4, 3, 2, 1};
   CHECK((solution->arraySign(nums) == 1));
 }
