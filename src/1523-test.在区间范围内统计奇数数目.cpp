561604171c59c92a394080455d54eb4c868bcdef
cosette <2052137445@qq.com>
2025-12-23 13:35:27 +0800

    1523.在区间范围内统计奇数数目

---
+++ src/1523-test.在区间范围内统计奇数数目.cpp
@@ -0,0 +1,9 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+namespace problem {
+#include "1523.在区间范围内统计奇数数目.cpp"
+}
+
+auto solution = new problem::Solution();
+
+TEST_CASE("1768") { CHECK((solution->countOdds(3, 7)) == 3); }
---
+++ src/1523.在区间范围内统计奇数数目.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=1523 lang=cpp
+ *
+ * [1523] 在区间范围内统计奇数数目
+ */
+
+// @lc code=start
+class Solution {
+public:
+  int countOdds(int low, int high) {
+    int count = 0;
+    for (int i = low; i <= high; i++) {
+      if (i % 2 != 0) {
+        count++;
+      }
+    }
+    return count;
+  }
+};
+// @lc code=end
